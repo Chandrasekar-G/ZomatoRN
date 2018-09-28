17fcb64d2b69600f0c11501877ffa1b72063e10c
Chandrasekar Gokulanathan <g.chandrasekar1993@gmail.com>
2018-09-28 15:18:21 +0530

    1.2 Login page design and styles

--- containers/LoginPage.js
+++ containers/LoginPage.js
@@ -1,11 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight,
+  Image,
+  Dimensions,
+  KeyboardAvoidingView,
+  StatusBar
+} from "react-native";
 
 export default class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  componentDidMount() {
+    StatusBar.setHidden(true);
+  }
+
+  login = () => {
+    console.log(this.state);
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Image
+          resizeMode="contain"
+          style={styles.logo}
+          source={require("../assets/logo.png")}
+        />
+        <KeyboardAvoidingView
+          style={styles.loginContainer}
+          behavior="padding"
+          enabled
+        >
+          <TextInput
+            style={styles.input}
+            placeholder="Email Id"
+            underlineColorAndroid="transparent"
+            onChangeText={email => this.setState({ email })}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={true}
+            underlineColorAndroid="transparent"
+            onChangeText={password => this.setState({ password })}
+          />
+          <TouchableHighlight
+            onPress={this.login}
+            style={styles.button}
+            underlayColor="#EC9EA9"
+          >
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableHighlight>
+        </KeyboardAvoidingView>
       </View>
     );
   }
@@ -14,8 +70,34 @@ export default class LoginPage extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundColor: "#F6E8EA"
   },
+  logo: {
+    width: Dimensions.get("window").width / 4,
+    alignSelf: "center"
+  },
+  loginContainer: {
+    marginLeft: 30,
+    marginRight: 30
+  },
+  input: {
+    height: 50,
+    paddingLeft: 10,
+    margin: 10,
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: "#E20025",
+    borderRadius: 10
+  },
+  button: {
+    backgroundColor: "#E20025",
+    padding: 10,
+    borderRadius: 10,
+    alignItems: "center"
+  },
+  buttonText: {
+    color: "#FFF"
+  }
 });
